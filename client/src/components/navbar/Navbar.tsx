73ee92b2801dac571a70492567e64a09dfbde41e
shirtol <shirzahavi7@gmail.com>
2022-07-03 12:49:59 +0300

    Added home to navbar

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { StyledHeaderWrapper } from "../layouts/StyledHeaderWrapper";
+import { StyledNavbar } from "./styles/StyledNavbar";
+import { StyledNavbarItem } from "./styles/StyledNavbarItem";
 
 const Navbar = () => {
-    return <StyledHeaderWrapper>Navbar</StyledHeaderWrapper>;
+    return (
+        <StyledHeaderWrapper>
+            <StyledNavbar>
+                <StyledNavbarItem>
+                    <NavLink to="/">Home</NavLink>
+                </StyledNavbarItem>
+            </StyledNavbar>
+        </StyledHeaderWrapper>
+    );
 };
 
 export default Navbar;
---
+++ client/src/components/navbar/styles/StyledNavbar.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import device from "../../../utils/stylesUtils/mediaQuerySizes";
+
+export const StyledNavbar = styled.header`
+    display: flex;
+    list-style: none;
+    gap: 3.5rem;
+    font-size: 2rem;
+    padding: 2rem;
+    @media ${device.tablet} {
+        display: none;
+    }
+`;
