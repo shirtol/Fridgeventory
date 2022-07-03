5f074b2ef8b1881accc8c27eef4693cf9011acaf
shirtol <shirzahavi7@gmail.com>
2022-07-03 14:46:11 +0300

    Added pages components

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,8 +1,22 @@
 import React from "react";
+import HoodFeed from "../../components/hoodFeed/HoodFeed";
+import ShoppingListHome from "../../components/shoppingListHome/ShoppingListHome";
+import { StyledFlexWrapper } from "../../components/layouts/StyledFlexWrapper";
 import { StyledMainWrapper } from "../../components/layouts/StyledMainWrapper";
+import ProductsCarousel from "../../components/productsCarousel/ProductsCarousel";
 
 const Home = () => {
-    return <StyledMainWrapper>Home</StyledMainWrapper>;
+    return (
+        <StyledMainWrapper>
+            <StyledFlexWrapper>
+                <ShoppingListHome></ShoppingListHome>
+                <StyledFlexWrapper>
+                    <HoodFeed></HoodFeed>
+                    <ProductsCarousel></ProductsCarousel>
+                </StyledFlexWrapper>
+            </StyledFlexWrapper>
+        </StyledMainWrapper>
+    );
 };
 
 export default Home;
