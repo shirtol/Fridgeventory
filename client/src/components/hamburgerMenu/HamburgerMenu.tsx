e04cf8d1c0b889dd80918fe888e22476f819ccfe
shirtol <shirzahavi7@gmail.com>
2022-07-14 19:18:02 +0300

    Handle edit product in hood and added humburger menu

---
+++ client/src/components/hamburgerMenu/HamburgerMenu.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { slide as Menu } from "react-burger-menu";
+import { NavLink } from "react-router-dom";
+import { useUser } from "../../context/userContext/User.context";
+import { StyledNavbarItem } from "../navbar/styles/StyledNavbarItem";
+import { styles } from "./styles/hamburgerMenuStyles";
+import "./styles/hamburgerMenuStyle.css";
+
+const HamburgerMenu = () => {
+    const [open, setOpen] = useState(false);
+    const { token } = useUser();
+
+    return (
+        <Menu
+            className="hamburger-menu"
+            styles={styles}
+            onOpen={() => setOpen(true)}
+            onClose={() => setOpen(false)}
+            isOpen={open}
+            // customBurgerIcon={<img src={imageData.avatar} alt="avatar" />}
+        >
+            <StyledNavbarItem onClick={() => setOpen(false)}>
+                <NavLink to="/">Home</NavLink>
+            </StyledNavbarItem>
+
+            <StyledNavbarItem onClick={() => setOpen(false)}>
+                <NavLink to="/fridge">Fridge</NavLink>
+            </StyledNavbarItem>
+
+            <StyledNavbarItem onClick={() => setOpen(false)}>
+                <NavLink to="/shopping-list">Shopping List</NavLink>
+            </StyledNavbarItem>
+
+            <StyledNavbarItem onClick={() => setOpen(false)}>
+                <NavLink to="/hood">Hood</NavLink>
+            </StyledNavbarItem>
+
+            <StyledNavbarItem onClick={() => setOpen(false)}>
+                <NavLink to="/statistics">Statistics</NavLink>
+            </StyledNavbarItem>
+
+            {!token && (
+                <StyledNavbarItem onClick={() => setOpen(false)}>
+                    <NavLink to="/register">Register</NavLink>
+                </StyledNavbarItem>
+            )}
+
+            {!token && (
+                <StyledNavbarItem onClick={() => setOpen(false)}>
+                    <NavLink to="/login">Login</NavLink>
+                </StyledNavbarItem>
+            )}
+
+            {token && (
+                <StyledNavbarItem onClick={() => setOpen(false)}>
+                    <NavLink to="/logout">Logout</NavLink>
+                </StyledNavbarItem>
+            )}
+        </Menu>
+    );
+};
+
+export default HamburgerMenu;
--- client/src/pages/routes/Routes.tsx
+++ client/src/pages/routes/Routes.tsx
@@ -1,4 +1,5 @@
 import { Route, Switch } from "react-router-dom";
+import HamburgerMenu from "../../components/hamburgerMenu/HamburgerMenu";
 import Background from "../../components/layouts/Background";
 import { StyledFlexWrapper } from "../../components/layouts/StyledFlexWrapper";
 import Navbar from "../../components/navbar/Navbar";
@@ -15,45 +16,48 @@ import Statistics from "../statistics/Statistics";
 
 const Routes = () => {
     return (
-        <StyledFlexWrapper
-            flexDirection="column"
-            height="100vh"
-            gap="0"
-            justifyContent="flex-start"
-        >
-            <Background></Background>
-            <WebsiteBanner></WebsiteBanner>
+        <>
             <StyledFlexWrapper
-                height="100%"
-                justifyContent="space-between"
-                alignItems="flex-start"
+                flexDirection="column"
+                height="100vh"
+                gap="0"
+                justifyContent="flex-start"
             >
-                <Navbar></Navbar>
-                <Switch>
-                    <Route exact path="/" component={Home}></Route>
-                    <Route exact path="/fridge" component={Fridge}></Route>
-                    <Route
-                        exact
-                        path="/shopping-list"
-                        component={ShoppingList}
-                    ></Route>
-                    <Route exact path="/hood" component={HoodPage}></Route>
-                    <Route
-                        exact
-                        path="/statistics"
-                        component={Statistics}
-                    ></Route>
-                    <Route
-                        exact
-                        path="/register"
-                        component={Registration}
-                    ></Route>
-                    <Route exact path="/login" component={Login}></Route>
-                    <Route exact path="/logout" component={Logout}></Route>
-                    <Route exact path="/my-hood" component={MyHood}></Route>
-                </Switch>
+                <Background></Background>
+                <WebsiteBanner></WebsiteBanner>
+                <StyledFlexWrapper
+                    height="100%"
+                    justifyContent="space-between"
+                    alignItems="flex-start"
+                >
+                    <HamburgerMenu></HamburgerMenu>
+                    <Navbar></Navbar>
+                    <Switch>
+                        <Route exact path="/" component={Home}></Route>
+                        <Route exact path="/fridge" component={Fridge}></Route>
+                        <Route
+                            exact
+                            path="/shopping-list"
+                            component={ShoppingList}
+                        ></Route>
+                        <Route exact path="/hood" component={HoodPage}></Route>
+                        <Route
+                            exact
+                            path="/statistics"
+                            component={Statistics}
+                        ></Route>
+                        <Route
+                            exact
+                            path="/register"
+                            component={Registration}
+                        ></Route>
+                        <Route exact path="/login" component={Login}></Route>
+                        <Route exact path="/logout" component={Logout}></Route>
+                        <Route exact path="/my-hood" component={MyHood}></Route>
+                    </Switch>
+                </StyledFlexWrapper>
             </StyledFlexWrapper>
-        </StyledFlexWrapper>
+        </>
     );
 };
 
