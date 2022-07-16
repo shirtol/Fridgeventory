14050f72e0d884e42f0568a639dbd4df9e57d416
shirtol <shirzahavi7@gmail.com>
2022-07-16 19:32:27 +0300

    Removing token when failing to logout

--- client/src/pages/logout/Logout.tsx
+++ client/src/pages/logout/Logout.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
 import HouseSpinner from "../../components/spinner/HouseSpinner";
 import { useUser } from "../../context/userContext/User.context";
 
 const Logout = () => {
     const { logout } = useUser();
     const [isLoading, setIsLoading] = useState(false);
+    const [cookies, setCookie, removeCookie] = useCookies(["token"]);
 
     useEffect(() => {
         setIsLoading(true);
@@ -13,6 +15,7 @@ const Logout = () => {
             try {
                 await logout!();
             } catch (err) {
+                removeCookie("token");
                 console.error(err);
             }
 
